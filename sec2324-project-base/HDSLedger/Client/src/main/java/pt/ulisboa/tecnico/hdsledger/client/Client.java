a945dea07447111a77600e0ff6e7bfebb70a67f5
pedricm <prmateus4@gmail.com>
2024-03-02 21:10:26 +0000

    client receives back messages

--- sec2324-project-base/HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/Client.java
+++ sec2324-project-base/HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/Client.java
@@ -1,5 +1,6 @@
 package pt.ulisboa.tecnico.hdsledger.client;
 import pt.ulisboa.tecnico.hdsledger.communication.Link;
+import pt.ulisboa.tecnico.hdsledger.communication.Message;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
 
 public class Client {
@@ -10,5 +11,19 @@ public class Client {
     public void send(String command) {
         System.out.println("Sending command to the library: " + command);
         clientLibrary.send(command);
+
+        new Thread(() -> {
+            Message message = null;
+            while (message == null) {
+                message = receive();
+            }
+        }).start();
+    }
+
+    public Message receive() {
+        Message message = this.clientLibrary.receive();
+        System.out.println("Node with ID: " + message.getSenderId() + " sent " + message.getType() + " for message: "
+            + message.getMessageId());
+        return message;
     }
 }
--- sec2324-project-base/HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/ClientLibrary.java
+++ sec2324-project-base/HDSLedger/Client/src/main/java/pt/ulisboa/tecnico/hdsledger/client/ClientLibrary.java
@@ -6,6 +6,8 @@ import pt.ulisboa.tecnico.hdsledger.communication.Message;
 import pt.ulisboa.tecnico.hdsledger.communication.ConsensusMessage;
 import pt.ulisboa.tecnico.hdsledger.communication.builder.ConsensusMessageBuilder;
 
+import java.io.IOException;
+
 
 public class ClientLibrary {
     //private static final CustomLogger LOGGER = new CustomLogger(ClientLibrary.class.getName());
@@ -14,6 +16,8 @@ public class ClientLibrary {
     private ProcessConfig nodeConfig;
     private ProcessConfig leaderConfig;
     private ProcessConfig[] nodeConfigs;
+
+    private static String nodeKeysPath = "../Service/src/main/resources/";
 
     public ClientLibrary(Link linkToNodes, ProcessConfig config, ProcessConfig leaderConf, ProcessConfig[] nodesConfig) {
         this.link = linkToNodes;
@@ -24,16 +28,24 @@ public class ClientLibrary {
 
     public void send(String msg) {
         System.out.println("Sending command: " + msg);
-        System.out.println("PrivKeyPath: " + nodeConfig.getPrivKeyPath());
-        System.out.println("PubKeyPath: " + nodeConfig.getPubKeyPath());
+
         //PrepareMessage prepareMessage = new PrepareMessage(prePrepareMessage.getValue());
 
         //ClientMessage clientMessage = new ClientMessageBuilder(nodeConfig.getId(), Message.Type.APPEND).setMessage(msg).setReplyTo(nodeConfig.getId()).setReplyToMessageId(0).build();
         ConsensusMessage consensusMessage = new ConsensusMessageBuilder(nodeConfig.getId(), Message.Type.APPEND)
                 .setMessage(msg).setReplyTo(nodeConfig.getId())
                 .setReplyToMessageId(0)
-                .setDS(nodeConfig.getPrivKeyPath())
+                .setDS(nodeKeysPath)
                 .build();
-        this.link.broadcast(consensusMessage);
+        this.link.send(leaderConfig.getId(), consensusMessage);
+    }
+
+    public Message receive() {
+        try {
+            return this.link.receive();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
